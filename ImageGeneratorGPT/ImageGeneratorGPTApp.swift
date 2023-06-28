2f911614a79eebba6f8aac34adb5243574d17b76
ynxgnk <nazar.iosdev@gmail.com>
2023-06-28 06:53:45 +0200

    first commit

---
+++ ImageGeneratorGPT/ImageGeneratorGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorGPTApp.swift
+//  ImageGeneratorGPT
+//
+//  Created by Nazar Kopeika on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
